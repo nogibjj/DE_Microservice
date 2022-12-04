1f79d2d9b72da8ad1c4d40668781ef474818e689
qihengGao <quintongao@gmail.com>
2022-12-04 21:11:44 +0000

    draft api

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+""" import neccessary libs"""
+from fastapi import FastAPI
+import uvicorn
+from mylib import user_request
+
+
+api = FastAPI()
+
+
+@api.get("/")
+async def root():
+    """Root page"""
+    return {"message": "Hello, this is an api to query info about World Cup!"}
+
+
+@api.get("/query/")
+def preprocess(request: user_request.QueryRequest):
+    """Do preprocessing"""
+    sql = request.query
+    print(sql)
+
+
 if __name__ == "__main__":
-    print("hello world!")
+    uvicorn.run(api, port=8080, host="0.0.0.0")
+
